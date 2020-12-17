63f2f1a1a53a3d1f4cc0ecb74c8d83f9db702bcb
Rusya <ruskakg@mail.ru>
2020-12-17 14:51:52 +0600

    library

--- Library/src/com/company/Juristic.java
+++ Library/src/com/company/Juristic.java
@@ -1,2 +1,44 @@
-package com.company;public class Juristic {
+package com.company;
+
+public class Juristic {
+    private String identifier;
+    private String author;
+    private int date;
+
+    public Juristic(){}
+
+    public void printInfo(){
+        System.out.println("Jurustic department: Identifier - " + identifier +
+                ", Author - " + author + ", Date - " + date);
+    }
+
+    public Juristic(String identifier, String author, int date) {
+        this.identifier = identifier;
+        this.author = author;
+        this.date = date;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
 }
--- Library/src/com/company/Main.java
+++ Library/src/com/company/Main.java
@@ -3,6 +3,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Library: ");
+        Artistic artistic = new Artistic("9789056622824", "Rob Perree", 2002);
+        Juristic juristic = new Juristic("9781138052840", "Roger Cotterrell", 2017);
+        Technical technical = new Technical("9781457501197", "Eric Evans", 2003);
+
+        Library lib = new Library();
+        lib.addBook(new Artistic("9780870701511", "Riva Castleman",1995));
+
+        System.out.println();
+        if(lib.addBook(artistic)){
+            System.out.println("Book was add");
+        }
+        System.out.println();
+        lib.printInfo();
     }
+
+
 }
